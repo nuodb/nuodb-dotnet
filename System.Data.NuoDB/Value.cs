a3d476fb2adc6c9a7a74c455be9254e3f11f59b1
Alberto Massari <amassari@nuodb.com>
2013-01-08 20:30:56 +0100

    Update copyright year

--- System.Data.NuoDB/Value.cs
+++ System.Data.NuoDB/Value.cs
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2012, NuoDB, Inc.
+* Copyright (c) 2012-2013, NuoDB, Inc.
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
