579af9bf60e14fb735eca7fd8d97537185a5a47e
Alberto Massari <amassari@nuodb.com>
2013-02-11 20:23:40 +0100

    Added ASP.NET sample

---
+++ WebApplicationSample/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="WebApplicationSample._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Welcome to NuoDB!
+    </h2>
+    <p>
+        To learn more about NuoDB visit <a href="http://www.nuodb.com" title="NuoDB Website">www.nuodb.com</a>.</p>
+    <p>
+        Select the position of the players: &nbsp;
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" 
+            onselectedindexchanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+    </p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </p>
+</asp:Content>
---
+++ WebApplicationSample/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationSample
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        hockeyEntities hockey;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            hockey = new hockeyEntities();
+
+            if (!IsPostBack)
+            {
+                var positions = from player in hockey.HOCKEY
+                                orderby player.NAME
+                                select new
+                                {
+                                    player.POSITION
+                                };
+                DropDownList1.DataValueField = "Position";
+                DropDownList1.DataTextField = "Position";
+                DropDownList1.DataSource = positions.Distinct();
+                DataBind();
+            }
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var players = from player in hockey.HOCKEY
+                          where player.POSITION == DropDownList1.SelectedValue
+                          orderby player.NAME
+                          select new
+                          {
+                              player.NAME,
+                              player.TEAM
+                          };
+            GridView1.DataSource = players;
+            DataBind();
+        }
+    }
+}
