947d8ae8e045333ec3945f623eed4e2047b01228
Alberto Massari <amassari@nuodb.com>
2013-05-20 22:28:13 +0200

    Added sample for a simple command line C# example

---
+++ ConsoleSample/HelloDB.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuoDb.Data.Client;
+using System.Data.Common;
+
+namespace ConsoleSample
+{
+    class HelloDB : IDisposable
+    {
+        private NuoDbConnection connection;
+
+        /*
+         * Creates an instance of HelloDB connected to the database with
+         * the given name on the localhost. This example class uses the
+         * default testing name & password.
+         */
+        public HelloDB(string dbName)
+        {
+            NuoDbConnectionStringBuilder builder = new NuoDbConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.Database = dbName;
+            builder.User = "dba";
+            builder.Password = "goalie";
+            builder.Schema = "hello";
+
+            this.connection = new NuoDbConnection(builder.ConnectionString);
+            this.connection.Open();
+        }
+
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (this.connection != null)
+                this.connection.Close();
+        }
+
+        #endregion
+
+        public void CreateTable()
+        {
+            try
+            {
+                using (DbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "create table names (id int generated always as identity primary key, name string)";
+                    command.ExecuteNonQuery();
+                    Console.Out.WriteLine("The table 'NAMES' was created.");
+                }
+            }
+            catch (NuoDbSqlException)
+            {
+                Console.Out.WriteLine("The table 'NAMES' already exists, re-using it.");
+            }
+        }
+
+        public void AddNames()
+        {
+            try
+            {
+                using (DbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "insert into names (name) values (?),(?),(?),(?),(?),(?),(?),(?),(?),(?),(?),(?),(?),(?),(?)";
+                    command.Prepare();
+
+                    /* batch insert a set of names */
+                    for (int i = 0; i < 15; i++) 
+                    {
+                        string name = String.Format("Fred # {0}", i+1);
+                        command.Parameters[i].Value = name;
+                    }
+                    // This is how to retrieve the generated key. Even if it's an update statement, we request the DbReader, that will contain 
+                    // only the generated column value
+                    using (DbDataReader reader = command.ExecuteReader())
+                    {
+                        string columnName = reader.GetName(0);
+                        while (reader.Read())
+                        {
+                            Console.Out.WriteLine("New id={0} for column {1}", reader.GetInt32(0), columnName);
+                        }
+                    }
+                }
+            }
+            catch (NuoDbSqlException e)
+            {
+                throw e;
+            }
+
+        }
+
+        public string GetName(int id)
+        {
+            try
+            {
+                using (DbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "select name from names where id=?";
+                    command.Parameters.Add(id);
+
+                    using (DbDataReader reader = command.ExecuteReader())
+                    {
+                        if(reader.Read())
+                            return reader.GetString(0);
+                    }
+                }
+                return null;
+            }
+            catch (NuoDbSqlException e)
+            {
+                throw e;
+            }
+
+        }
+    }
+}
---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuoDb.Data.Client;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Count() != 1) 
+            {
+                Console.Out.WriteLine("Usage: ConsoleSample.exe <dbname>\n");
+                return 1;
+            }
+
+            try {
+                HelloDB helloDB = new HelloDB(args[0]);
+
+                helloDB.CreateTable();
+                helloDB.AddNames();
+                string name = helloDB.GetName(12);
+
+                Console.Out.WriteLine("The NAME with id of 12 is '{0}'\n", name);
+
+            }
+            catch (Exception xcp)
+            {
+                Console.Out.WriteLine("Got exception: {0}\n", xcp.Message);
+                return 2;
+            }
+
+            return 0;
+        }
+    }
+}
