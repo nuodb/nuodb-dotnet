acf2de84b6de6aeff262e159a4f26304b58a115f
Jiri Cincura <jiri@x2develop.com>
2013-06-12 16:42:51 +0200

    Cleanup implementation.

--- NuoDb.Data.Client/ConnectionPoolManager.cs
+++ NuoDb.Data.Client/ConnectionPoolManager.cs
@@ -6,25 +6,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace NuoDb.Data.Client
 {
-	internal class ConnectionPoolManager
+	internal sealed class ConnectionPoolManager
 	{
-		internal static ConnectionPoolManager Instance = new ConnectionPoolManager();
+		static ConnectionPoolManager _instance = new ConnectionPoolManager();
+		public static ConnectionPoolManager Instance { get { return _instance; } }
 
 		sealed class ConnectionPool
 		{
+			sealed class Item
+			{
+				public DateTimeOffset Created { get; private set; }
+				public NuoDbConnectionInternal Connection { get; private set; }
+
+				public Item(DateTimeOffset created, NuoDbConnectionInternal connection)
+				{
+					Created = created;
+					Connection = connection;
+				}
+			}
+
 			object _syncRoot;
 			string _connectionString;
-			Queue<NuoDbConnectionInternal> _available;
+			TimeSpan _lifeTime;
+			Queue<Item> _available;
 			List<NuoDbConnectionInternal> _busy;
 
 			public ConnectionPool(string connectionString)
 			{
 				_syncRoot = new object();
 				_connectionString = connectionString;
-				_available = new Queue<NuoDbConnectionInternal>();
+				_lifeTime = TimeSpan.FromSeconds(new NuoDbConnectionStringBuilder(_connectionString).ConnectionLifetimeOrDefault);
+				_available = new Queue<Item>();
 				_busy = new List<NuoDbConnectionInternal>();
 			}
 
@@ -33,7 +49,7 @@ namespace NuoDb.Data.Client
 				lock (_syncRoot)
 				{
 					var connection = _available.Count() > 0
-						? _available.Dequeue()
+						? _available.Dequeue().Connection
 						: new NuoDbConnectionInternal(_connectionString);
 					_busy.Add(connection);
 					return connection;
@@ -44,32 +60,42 @@ namespace NuoDb.Data.Client
 			{
 				lock (_syncRoot)
 				{
-					_busy.Remove(connection);
-					_available.Enqueue(connection);
+					var removed = _busy.Remove(connection);
+					if (removed)
+						_available.Enqueue(new Item(DateTimeOffset.UtcNow, connection));
+				}
+			}
+
+			public void CleanupPool()
+			{
+				lock (_syncRoot)
+				{
+					var now = DateTimeOffset.UtcNow;
+					var available = _available.ToArray();
+					var keep = available.Where(x => x.Created.Add(_lifeTime) > now);
+					var release = available.Except(keep);
+					release.AsParallel().ForAll(x => x.Connection.Dispose());
+					_available = new Queue<Item>(keep);
 				}
 			}
 		}
 
 		object _syncRoot = new object();
 		Dictionary<string, ConnectionPool> _pools;
+		Timer _cleanupTimer;
 
 		ConnectionPoolManager()
 		{
 			_syncRoot = new object();
 			_pools = new Dictionary<string, ConnectionPool>();
+			_cleanupTimer = new Timer(CleanupCallback, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(10));
 		}
 
 		public NuoDbConnectionInternal Get(string connectionString)
 		{
 			lock (_syncRoot)
 			{
-				var pool = default(ConnectionPool);
-				if (!_pools.TryGetValue(connectionString, out pool))
-				{
-					pool = new ConnectionPool(connectionString);
-					_pools.Add(connectionString, pool);
-				}
-				return pool.GetConnection();
+				return GetPoolOrCreateNew(connectionString).GetConnection();
 			}
 		}
 
@@ -77,10 +103,37 @@ namespace NuoDb.Data.Client
 		{
 			lock (_syncRoot)
 			{
-				var pool = default(ConnectionPool);
-				if (!_pools.TryGetValue(connection.ConnectionString, out pool))
-					throw new InvalidOperationException("This connection was not taken from pool.");
-				pool.ReleaseConnection(connection);
+				GetPoolOrCreateNew(connection.ConnectionString).ReleaseConnection(connection);
+			}
+		}
+
+		ConnectionPool GetPoolOrCreateNew(string connectionString)
+		{
+			var pool = default(ConnectionPool);
+			if (!_pools.TryGetValue(connectionString, out pool))
+			{
+				pool = PrepareNewPool(connectionString);
+			}
+			return pool;
+		}
+
+		ConnectionPool PrepareNewPool(string connectionString)
+		{
+			var pool = new ConnectionPool(connectionString);
+			_pools.Add(connectionString, pool);
+			return pool;
+		}
+
+		void CleanupCallback(object o)
+		{
+			Cleanup();
+		}
+
+		void Cleanup()
+		{
+			lock (_syncRoot)
+			{
+				_pools.Values.AsParallel().ForAll(p => p.CleanupPool());
 			}
 		}
 	}
