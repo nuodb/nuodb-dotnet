a66559e865d6bd6cc26dde18a7daf0fda27edab1
Jiri Cincura <jiri@x2develop.com>
2013-06-12 11:03:28 +0200

    Using constants.

--- NuoDb.Data.Client/NuoDbConnectionStringBuilder.cs
+++ NuoDb.Data.Client/NuoDbConnectionStringBuilder.cs
@@ -24,6 +24,9 @@
 * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+* Contributors:
+*	Jiri Cincura (jiri@cincura.net)
 ****************************************************************************/
 
 using System.Data.Common;
@@ -32,98 +35,105 @@ using System.ComponentModel;
 
 namespace NuoDb.Data.Client
 {
-    public class NuoDbConnectionStringBuilder : DbConnectionStringBuilder
-    {
-        [Category("Security")]
-        [Description("Indicates the username to be used when connecting to the data source")]
-        public string User
-        {
-            get { return this.GetString("User"); }
-            set { this.SetValue("User", value); }
-        }
+	public class NuoDbConnectionStringBuilder : DbConnectionStringBuilder
+	{
+		internal const string UserKey = "User";
+		[Category("Security")]
+		[Description("Indicates the username to be used when connecting to the data source")]
+		public string User
+		{
+			get { return this.GetString(UserKey); }
+			set { this.SetValue(UserKey, value); }
+		}
 
-        [Category("Security")]
-        [Description("Indicates the password to be used when connecting to the data source")]
-        [PasswordPropertyText(true)]
-        public string Password
-        {
-            get { return this.GetString("Password"); }
-            set { this.SetValue("Password", value); }
-        }
+		internal const string PasswordKey = "Password";
+		[Category("Security")]
+		[Description("Indicates the password to be used when connecting to the data source")]
+		[PasswordPropertyText(true)]
+		public string Password
+		{
+			get { return this.GetString(PasswordKey); }
+			set { this.SetValue(PasswordKey, value); }
+		}
 
-        [Category("Source")]
-        [Description("Indicates one or more addresses of the brokers to be contacted, separated by commas")]
-        public string Server
-        {
-            get { return this.GetString("Server"); }
-            set { this.SetValue("Server", value); }
-        }
+		internal const string ServerKey = "Server";
+		[Category("Source")]
+		[Description("Indicates one or more addresses of the brokers to be contacted, separated by commas")]
+		public string Server
+		{
+			get { return this.GetString(ServerKey); }
+			set { this.SetValue(ServerKey, value); }
+		}
 
-        [Category("Source")]
-        [Description("Indicates the chorus to be used as data source")]
-        public string Database
-        {
-            get { return this.GetString("Database"); }
-            set { this.SetValue("Database", value); }
-        }
+		internal const string DatabaseKey = "Database";
+		[Category("Source")]
+		[Description("Indicates the chorus to be used as data source")]
+		public string Database
+		{
+			get { return this.GetString(DatabaseKey); }
+			set { this.SetValue(DatabaseKey, value); }
+		}
 
-        [Category("Source")]
-        [Description("Indicates the default schema to be used when referring to non-fully qualified tables")]
-        public string Schema
-        {
-            get { return this.GetString("Schema"); }
-            set { this.SetValue("Schema", value); }
-        }
+		internal const string SchemaKey = "Schema";
+		[Category("Source")]
+		[Description("Indicates the default schema to be used when referring to non-fully qualified tables")]
+		public string Schema
+		{
+			get { return this.GetString(SchemaKey); }
+			set { this.SetValue(SchemaKey, value); }
+		}
 
+		internal const string PoolingKey = "Pooling";
 		[Category("Connection Pooling")]
 		[Description("")]
 		public bool Pooling
 		{
-			get { return this.GetBoolean("Pooling"); }
-			set { this.SetValue("Pooling", value); }
+			get { return this.GetBoolean(PoolingKey); }
+			set { this.SetValue(PoolingKey, value); }
 		}
 
+		internal const string ConnectionLifetimeKey = "ConnectionLifetime";
 		[Category("Connection Pooling")]
 		[Description("")]
 		public int ConnectionLifetime
 		{
-			get { return this.GetInt32("ConnectionLifetime"); }
-			set { this.SetValue("ConnectionLifetime", value); }
+			get { return this.GetInt32(ConnectionLifetimeKey); }
+			set { this.SetValue(ConnectionLifetimeKey, value); }
 		}
 
-        public NuoDbConnectionStringBuilder()
+		public NuoDbConnectionStringBuilder()
 		{
 		}
 
-        public NuoDbConnectionStringBuilder(string connectionString)
+		public NuoDbConnectionStringBuilder(string connectionString)
 		{
 			this.ConnectionString = connectionString;
 		}
 
-        private string GetString(string keyword)
-        {
+		string GetString(string keyword)
+		{
 			return GetValue(keyword, Convert.ToString);
-        }
+		}
 
-		private bool GetBoolean(string keyword)
+		bool GetBoolean(string keyword)
 		{
 			return GetValue(keyword, Convert.ToBoolean);
 		}
 
-		private int GetInt32(string keyword)
+		int GetInt32(string keyword)
 		{
 			return GetValue(keyword, Convert.ToInt32);
 		}
 
-		private T GetValue<T>(string keyword, Converter<object, T> converter)
+		T GetValue<T>(string keyword, Converter<object, T> converter)
 		{
 			return converter(this[keyword]);
 		}
 
-        private void SetValue<T>(string keyword, T value)
-        {
-            this[keyword] = value;
-        }
+		void SetValue<T>(string keyword, T value)
+		{
+			this[keyword] = value;
+		}
 
-    }
+	}
 }
