336b38cf3f958b6c7035e629b7539e0e785e4b2f
Jiri Cincura <jiri@x2develop.com>
2013-06-19 16:08:28 +0200

    Allowed INTERSECT and EXCEPT.

--- NuoDb.Data.Client/EntityFramework/SqlGen/SqlGenerator.cs
+++ NuoDb.Data.Client/EntityFramework/SqlGen/SqlGenerator.cs
@@ -734,8 +734,7 @@ namespace NuoDb.Data.Client.EntityFramework.SqlGen
 		/// <returns></returns>
 		public override ISqlFragment Visit(DbExceptExpression e)
 		{
-			throw new NotSupportedException("The EXCEPT statement is not supported in NuoDB.");
-			//return VisitSetOpExpression(e.Left, e.Right, "EXCEPT");
+			return VisitSetOpExpression(e.Left, e.Right, "EXCEPT");
 		}
 
 		/// <summary>
@@ -1073,8 +1072,7 @@ namespace NuoDb.Data.Client.EntityFramework.SqlGen
 		/// <returns></returns>
 		public override ISqlFragment Visit(DbIntersectExpression e)
 		{
-			throw new NotSupportedException("The INTERSECT statement is not supported in NuoDB.");
-			//return VisitSetOpExpression(e.Left, e.Right, "INTERSECT");
+			return VisitSetOpExpression(e.Left, e.Right, "INTERSECT");
 		}
 
 		/// <summary>
