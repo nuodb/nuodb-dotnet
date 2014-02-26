cc60d2742f0b76e20ce54766c30853c91d4e9826
Jiri Cincura <jiri@x2develop.com>
2014-02-26 11:11:30 +0100

    Matching resource names.

--- NuoDb.Data.Client/EntityFramework/NuoDbProviderManifest.cs
+++ NuoDb.Data.Client/EntityFramework/NuoDbProviderManifest.cs
@@ -48,179 +48,194 @@ namespace EntityFramework.NuoDb
 namespace NuoDb.Data.Client.EntityFramework
 #endif
 {
-    class NuoDbProviderManifest : DbXmlEnabledProviderManifest
-    {
-        internal const int BinaryMaxSize = Int32.MaxValue;
-        internal const int VarcharMaxSize = 32765;
-        internal const int NVarcharMaxSize = VarcharMaxSize;
-        internal const char LikeEscapeCharacter = '\\';
+	class NuoDbProviderManifest : DbXmlEnabledProviderManifest
+	{
+		const string ProviderManifestResourceName =
+#if EF6
+ "EntityFramework.NuoDb.ProviderManifest.xml";
+#else
+ "NuoDb.Data.Client.EntityFramework.ProviderManifest.xml";
+#endif
+		const string StoreSchemaDefinitionResourceName =
+#if EF6
+ "EntityFramework.NuoDb.StoreSchemaDefinition.ssdl";
+#else
+ "NuoDb.Data.Client.EntityFramework.StoreSchemaDefinition.ssdl";
+#endif
+		const string StoreSchemaMappingResourceName =
+#if EF6
+ "EntityFramework.NuoDb.StoreSchemaMapping.msl";
+#else
+ "NuoDb.Data.Client.EntityFramework.StoreSchemaMapping.msl";
+#endif
 
-        public NuoDbProviderManifest()
-#warning Resource name
-            : base(XmlReader.Create(typeof(NuoDbProviderManifest).Assembly.GetManifestResourceStream("NuoDb.Data.Client.EntityFramework.ProviderManifest.xml")))
-        {
-        }
+		internal const int BinaryMaxSize = Int32.MaxValue;
+		internal const int VarcharMaxSize = 32765;
+		internal const int NVarcharMaxSize = VarcharMaxSize;
+		internal const char LikeEscapeCharacter = '\\';
 
-        protected override XmlReader GetDbInformation(string informationType)
-        {
+		public NuoDbProviderManifest()
+			: base(XmlReader.Create(typeof(NuoDbProviderManifest).Assembly.GetManifestResourceStream(ProviderManifestResourceName)))
+		{ }
+
+		protected override XmlReader GetDbInformation(string informationType)
+		{
 #warning V3 schema
-            if (informationType == DbProviderManifest.StoreSchemaDefinition)
+			if (informationType == DbProviderManifest.StoreSchemaDefinition)
 			{
-#warning Resource name
-                return XmlReader.Create(typeof(NuoDbProviderManifest).Assembly.GetManifestResourceStream("NuoDb.Data.Client.EntityFramework.StoreSchemaDefinition.ssdl"));
+				return XmlReader.Create(typeof(NuoDbProviderManifest).Assembly.GetManifestResourceStream(StoreSchemaDefinitionResourceName));
 			}
 #warning V3 schema
-            else if (informationType == DbProviderManifest.StoreSchemaMapping)
+			else if (informationType == DbProviderManifest.StoreSchemaMapping)
 			{
-#warning Resource name
-                return XmlReader.Create(typeof(NuoDbProviderManifest).Assembly.GetManifestResourceStream("NuoDb.Data.Client.EntityFramework.StoreSchemaMapping.msl"));
-            }
+				return XmlReader.Create(typeof(NuoDbProviderManifest).Assembly.GetManifestResourceStream(StoreSchemaMappingResourceName));
+			}
 #warning CSDL
-            throw new NotImplementedException();
-        }
+			throw new NotImplementedException();
+		}
 
-        public override string NamespaceName
-        {
-            get { return "NuoDB"; }
-        }
+		public override string NamespaceName
+		{
+			get { return "NuoDB"; }
+		}
 
-        public override TypeUsage GetEdmType(TypeUsage storeType)
-        {
-            string storeTypeName = storeType.EdmType.Name.ToLower();
-            PrimitiveType edmPrimitiveType = base.StoreTypeNameToEdmPrimitiveType[storeTypeName];
-            if (storeTypeName == "string" || storeTypeName == "varchar")
-            {
-                Facet f;
-                int maxLength = -1;
-                if (storeType.Facets.TryGetValue("MaxLength", false, out f) && !f.IsUnbounded && f.Value != null)
-                    maxLength = (int)f.Value;
-                if (maxLength != -1)
-                    return TypeUsage.CreateStringTypeUsage(edmPrimitiveType, false, false, maxLength);
-                else
-                    return TypeUsage.CreateStringTypeUsage(edmPrimitiveType, false, false);
-            }
-            else if (storeTypeName == "decimal" || storeTypeName == "numeric")
-            {
-                Facet f;
-                byte precision = 0;
-                if (storeType.Facets.TryGetValue("Precision", false, out f) && !f.IsUnbounded && f.Value != null)
-                    precision = (byte)f.Value;
-                byte scale = 0;
-                if (storeType.Facets.TryGetValue("Scale", false, out f) && !f.IsUnbounded && f.Value != null)
-                    scale = (byte)f.Value;
-                if (precision != 0 && scale != 0)
-                    return TypeUsage.CreateDecimalTypeUsage(edmPrimitiveType, precision, scale);
-                else
-                    return TypeUsage.CreateDecimalTypeUsage(edmPrimitiveType);
-            }
-            else
-                return TypeUsage.CreateDefaultTypeUsage(edmPrimitiveType);
-            throw new NotImplementedException();
-        }
+		public override TypeUsage GetEdmType(TypeUsage storeType)
+		{
+			string storeTypeName = storeType.EdmType.Name.ToLower();
+			PrimitiveType edmPrimitiveType = base.StoreTypeNameToEdmPrimitiveType[storeTypeName];
+			if (storeTypeName == "string" || storeTypeName == "varchar")
+			{
+				Facet f;
+				int maxLength = -1;
+				if (storeType.Facets.TryGetValue("MaxLength", false, out f) && !f.IsUnbounded && f.Value != null)
+					maxLength = (int)f.Value;
+				if (maxLength != -1)
+					return TypeUsage.CreateStringTypeUsage(edmPrimitiveType, false, false, maxLength);
+				else
+					return TypeUsage.CreateStringTypeUsage(edmPrimitiveType, false, false);
+			}
+			else if (storeTypeName == "decimal" || storeTypeName == "numeric")
+			{
+				Facet f;
+				byte precision = 0;
+				if (storeType.Facets.TryGetValue("Precision", false, out f) && !f.IsUnbounded && f.Value != null)
+					precision = (byte)f.Value;
+				byte scale = 0;
+				if (storeType.Facets.TryGetValue("Scale", false, out f) && !f.IsUnbounded && f.Value != null)
+					scale = (byte)f.Value;
+				if (precision != 0 && scale != 0)
+					return TypeUsage.CreateDecimalTypeUsage(edmPrimitiveType, precision, scale);
+				else
+					return TypeUsage.CreateDecimalTypeUsage(edmPrimitiveType);
+			}
+			else
+				return TypeUsage.CreateDefaultTypeUsage(edmPrimitiveType);
+			throw new NotImplementedException();
+		}
 
-        public override TypeUsage GetStoreType(TypeUsage edmType)
-        {
-            if (edmType == null)
-                throw new ArgumentNullException("edmType");
+		public override TypeUsage GetStoreType(TypeUsage edmType)
+		{
+			if (edmType == null)
+				throw new ArgumentNullException("edmType");
 
-            System.Diagnostics.Debug.Assert(edmType.EdmType.BuiltInTypeKind == BuiltInTypeKind.PrimitiveType);
+			System.Diagnostics.Debug.Assert(edmType.EdmType.BuiltInTypeKind == BuiltInTypeKind.PrimitiveType);
 
-            PrimitiveType primitiveType = edmType.EdmType as PrimitiveType;
-            if (primitiveType == null)
-                throw new ArgumentException(String.Format("The underlying provider does not support the type '{0}'.", edmType));
+			PrimitiveType primitiveType = edmType.EdmType as PrimitiveType;
+			if (primitiveType == null)
+				throw new ArgumentException(String.Format("The underlying provider does not support the type '{0}'.", edmType));
 
-            ReadOnlyMetadataCollection<Facet> facets = edmType.Facets;
-            Facet f;
+			ReadOnlyMetadataCollection<Facet> facets = edmType.Facets;
+			Facet f;
 
-            switch (primitiveType.PrimitiveTypeKind)
-            {
-                case PrimitiveTypeKind.Boolean:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["boolean"]);
+			switch (primitiveType.PrimitiveTypeKind)
+			{
+				case PrimitiveTypeKind.Boolean:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["boolean"]);
 
-                case PrimitiveTypeKind.Byte:
-                case PrimitiveTypeKind.Int16:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["smallint"]);
+				case PrimitiveTypeKind.Byte:
+				case PrimitiveTypeKind.Int16:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["smallint"]);
 
-                case PrimitiveTypeKind.Int32:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["integer"]);
+				case PrimitiveTypeKind.Int32:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["integer"]);
 
-                case PrimitiveTypeKind.Int64:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["bigint"]);
+				case PrimitiveTypeKind.Int64:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["bigint"]);
 
-                case PrimitiveTypeKind.Double:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["double"]);
+				case PrimitiveTypeKind.Double:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["double"]);
 
-                case PrimitiveTypeKind.Single:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["float"]);
+				case PrimitiveTypeKind.Single:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["float"]);
 
-                case PrimitiveTypeKind.Decimal: // decimal, numeric
-                    {
-                        byte precision = 9;
-                        if (facets.TryGetValue("Precision", false, out f) && !f.IsUnbounded && f.Value != null)
-                            precision = (byte)f.Value;
-                        byte scale = 0;
-                        if (facets.TryGetValue("Scale", false, out f) && !f.IsUnbounded && f.Value != null)
-                            scale = (byte)f.Value;
+				case PrimitiveTypeKind.Decimal: // decimal, numeric
+					{
+						byte precision = 9;
+						if (facets.TryGetValue("Precision", false, out f) && !f.IsUnbounded && f.Value != null)
+							precision = (byte)f.Value;
+						byte scale = 0;
+						if (facets.TryGetValue("Scale", false, out f) && !f.IsUnbounded && f.Value != null)
+							scale = (byte)f.Value;
 
-                        return TypeUsage.CreateDecimalTypeUsage(StoreTypeNameToStorePrimitiveType["decimal"], precision, scale);
-                    }
+						return TypeUsage.CreateDecimalTypeUsage(StoreTypeNameToStorePrimitiveType["decimal"], precision, scale);
+					}
 
-                case PrimitiveTypeKind.String: // char, varchar, text blob
-                    {
-                        bool isUnicode = true;
-                        if (facets.TryGetValue("Unicode", false, out f) && !f.IsUnbounded && f.Value != null)
-                            isUnicode = (bool)f.Value;
-                        bool isFixedLength = true;
-                        if (facets.TryGetValue("FixedLength", false, out f) && !f.IsUnbounded && f.Value != null)
-                            isFixedLength = (bool)f.Value;
-                        int maxLength = Int32.MinValue;
-                        if (facets.TryGetValue("MaxLength", false, out f) && !f.IsUnbounded && f.Value != null)
-                            maxLength = (int)f.Value;
+				case PrimitiveTypeKind.String: // char, varchar, text blob
+					{
+						bool isUnicode = true;
+						if (facets.TryGetValue("Unicode", false, out f) && !f.IsUnbounded && f.Value != null)
+							isUnicode = (bool)f.Value;
+						bool isFixedLength = true;
+						if (facets.TryGetValue("FixedLength", false, out f) && !f.IsUnbounded && f.Value != null)
+							isFixedLength = (bool)f.Value;
+						int maxLength = Int32.MinValue;
+						if (facets.TryGetValue("MaxLength", false, out f) && !f.IsUnbounded && f.Value != null)
+							maxLength = (int)f.Value;
 
-                        PrimitiveType storePrimitiveType = StoreTypeNameToStorePrimitiveType[isFixedLength ? "char" : "varchar"];
-                        if (maxLength != Int32.MinValue)
-                            return TypeUsage.CreateStringTypeUsage(storePrimitiveType, isUnicode, isFixedLength, maxLength);
+						PrimitiveType storePrimitiveType = StoreTypeNameToStorePrimitiveType[isFixedLength ? "char" : "varchar"];
+						if (maxLength != Int32.MinValue)
+							return TypeUsage.CreateStringTypeUsage(storePrimitiveType, isUnicode, isFixedLength, maxLength);
 
-                        return TypeUsage.CreateStringTypeUsage(storePrimitiveType, isUnicode, isFixedLength);
-                    }
+						return TypeUsage.CreateStringTypeUsage(storePrimitiveType, isUnicode, isFixedLength);
+					}
 
-                case PrimitiveTypeKind.DateTime: // datetime, date
-                    {
-                        byte precision = 4;
-                        if (facets.TryGetValue("Precision", false, out f) && !f.IsUnbounded && f.Value != null)
-                            precision = (byte)f.Value;
+				case PrimitiveTypeKind.DateTime: // datetime, date
+					{
+						byte precision = 4;
+						if (facets.TryGetValue("Precision", false, out f) && !f.IsUnbounded && f.Value != null)
+							precision = (byte)f.Value;
 
-                        bool useTimestamp = (precision != 0);
+						bool useTimestamp = (precision != 0);
 
-                        return TypeUsage.CreateDefaultTypeUsage(useTimestamp ? StoreTypeNameToStorePrimitiveType["datetime"] : StoreTypeNameToStorePrimitiveType["date"]);
-                    }
+						return TypeUsage.CreateDefaultTypeUsage(useTimestamp ? StoreTypeNameToStorePrimitiveType["datetime"] : StoreTypeNameToStorePrimitiveType["date"]);
+					}
 
-                case PrimitiveTypeKind.Time:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["time"]);
+				case PrimitiveTypeKind.Time:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["time"]);
 
-                case PrimitiveTypeKind.Binary: // blob
-                    {
-                        bool isFixedLength = true;
-                        if (facets.TryGetValue("FixedLength", false, out f) && !f.IsUnbounded && f.Value != null)
-                            isFixedLength = (bool)f.Value;
-                        int maxLength = Int32.MinValue;
-                        if (facets.TryGetValue("MaxLength", false, out f) && !f.IsUnbounded && f.Value != null)
-                            maxLength = (int)f.Value;
+				case PrimitiveTypeKind.Binary: // blob
+					{
+						bool isFixedLength = true;
+						if (facets.TryGetValue("FixedLength", false, out f) && !f.IsUnbounded && f.Value != null)
+							isFixedLength = (bool)f.Value;
+						int maxLength = Int32.MinValue;
+						if (facets.TryGetValue("MaxLength", false, out f) && !f.IsUnbounded && f.Value != null)
+							maxLength = (int)f.Value;
 
-                        PrimitiveType storePrimitiveType = StoreTypeNameToStorePrimitiveType["binary"];
-                        if (maxLength != Int32.MinValue)
-                            return TypeUsage.CreateBinaryTypeUsage(storePrimitiveType, isFixedLength, maxLength);
+						PrimitiveType storePrimitiveType = StoreTypeNameToStorePrimitiveType["binary"];
+						if (maxLength != Int32.MinValue)
+							return TypeUsage.CreateBinaryTypeUsage(storePrimitiveType, isFixedLength, maxLength);
 
-                        return TypeUsage.CreateBinaryTypeUsage(storePrimitiveType, isFixedLength);
-                    }
+						return TypeUsage.CreateBinaryTypeUsage(storePrimitiveType, isFixedLength);
+					}
 
-                case PrimitiveTypeKind.Guid:
-                    return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["guid_char"]);
+				case PrimitiveTypeKind.Guid:
+					return TypeUsage.CreateDefaultTypeUsage(StoreTypeNameToStorePrimitiveType["guid_char"]);
 
-                default:
-                    throw new NotSupportedException(string.Format("There is no store type corresponding to the EDM type '{0}' of primitive type '{1}'.", edmType, primitiveType.PrimitiveTypeKind));
-            }
-        }
+				default:
+					throw new NotSupportedException(string.Format("There is no store type corresponding to the EDM type '{0}' of primitive type '{1}'.", edmType, primitiveType.PrimitiveTypeKind));
+			}
+		}
 
 #if NET_40
         public override bool SupportsEscapingLikeArgument(out char escapeCharacter)
@@ -239,7 +254,7 @@ namespace NuoDb.Data.Client.EntityFramework
         }
 #endif
 
-    }
+	}
 }
 
 #endif
