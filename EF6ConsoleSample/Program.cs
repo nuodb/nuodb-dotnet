2059e1362d40880efd7bd91da5160e1042835fba
Alberto Massari <amassari@nuodb.com>
2014-03-10 21:03:56 +0100

    - removed EF binaries from GIT, let NuGet download them locally - make the EF6Console project look for the binaries where they are placed in the installation folder, and added a .user file (not to be installed) to pick the locations in the build tree - fixed the app.config file to reference NuoDb, not SQL Server

--- EF6ConsoleSample/Program.cs
+++ EF6ConsoleSample/Program.cs
@@ -21,7 +21,7 @@ namespace EF6ConsoleSample
     class NuoDbDirectContext : DbContext, ISomeNuoDbDbContext
     {
         public NuoDbDirectContext()
-            : base(new NuoDbConnection("Server=localhost;Database=quickstart;User=dba;Password=goalie;Schema=hello"), true)
+            : base(new NuoDbConnection("Server=localhost;Database=test;User=dba;Password=goalie;Schema=hello"), true)
         { }
 
         public DbSet<FooBar> FooBars { get; set; }
@@ -60,19 +60,27 @@ namespace EF6ConsoleSample
         
         static void Main(string[] args)
         {
-            using (var ctx = GetContext())
+            try
             {
-                Console.WriteLine("Using Entity Framework version: "+ctx.GetType().BaseType.Assembly.FullName);
+                using (var ctx = GetContext())
+                {
+                    Console.WriteLine("Using Entity Framework version: " + ctx.GetType().BaseType.Assembly.FullName);
 
-                var query1 = ctx.FooBars.Where(x => x.Id == 666);
-                Console.WriteLine("Linq query  : ctx.FooBars.Where(x => x.Id == 666)");
-                Console.WriteLine("-> SQL query:");
-                Console.WriteLine(query1);
-                Console.WriteLine();
-                var query2 = ctx.FooBars.Where(x => new[] { 1, 2, 3 }.Contains(x.Id));
-                Console.WriteLine("Linq query  : ctx.FooBars.Where(x => new[] { 1, 2, 3 }.Contains(x.Id))");
-                Console.WriteLine("-> SQL query:");
-                Console.WriteLine(query2);
+                    var query1 = ctx.FooBars.Where(x => x.Id == 666);
+                    Console.WriteLine("Linq query  : ctx.FooBars.Where(x => x.Id == 666)");
+                    Console.WriteLine("-> SQL query:");
+                    Console.WriteLine(query1);
+                    Console.WriteLine();
+                    var query2 = ctx.FooBars.Where(x => new[] { 1, 2, 3 }.Contains(x.Id));
+                    Console.WriteLine("Linq query  : ctx.FooBars.Where(x => new[] { 1, 2, 3 }.Contains(x.Id))");
+                    Console.WriteLine("-> SQL query:");
+                    Console.WriteLine(query2);
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.Out.WriteLine(e.Message);
+                System.Console.Out.WriteLine(e.StackTrace);
             }
         }
     }
